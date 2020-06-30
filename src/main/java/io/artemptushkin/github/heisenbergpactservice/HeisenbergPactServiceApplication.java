c9b6bafab10532d4d8f4acb582f0baf80ace8bea
Artem Ptushkin <artem.ptushkin@ev-box.com>
2020-06-30 16:42:10 +0300

    Initial commit

---
+++ src/main/java/io/artemptushkin/github/heisenbergpactservice/HeisenbergPactServiceApplication.java
@@ -0,0 +1,13 @@
+package io.artemptushkin.github.heisenbergpactservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeisenbergPactServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeisenbergPactServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/artemptushkin/github/heisenbergpactservice/HeisenbergPactServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.artemptushkin.github.heisenbergpactservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeisenbergPactServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
