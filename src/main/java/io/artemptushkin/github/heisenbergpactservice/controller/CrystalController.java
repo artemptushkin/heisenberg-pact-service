1c80b370667e6dc2cb115cb56fa11635dae5547f
artemptushkin <artem.ptushkin@ev-box.com>
2021-04-26 09:36:40 +0200

    scenario-1: before consumers pr

--- src/main/java/io/artemptushkin/github/heisenbergpactservice/controller/CrystalController.java
+++ src/main/java/io/artemptushkin/github/heisenbergpactservice/controller/CrystalController.java
@@ -1,8 +1,5 @@
 package io.artemptushkin.github.heisenbergpactservice.controller;
 
-import java.math.BigDecimal;
-import java.util.List;
-
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,23 +14,6 @@ public class CrystalController {
 
 	@GetMapping
 	public CrystalsResponse get(@RequestParam Integer amount) {
-		BigDecimal price = new BigDecimal("10.0");
-		List<Crystal> crystals = List.of(
-				Crystal
-						.builder()
-						.color("red")
-						.id(1L)
-						.build(),
-				Crystal
-						.builder()
-						.color("blue")
-						.id(2L)
-						.build()
-		);
-		return CrystalsResponse
-				.builder()
-				.crystals(crystals)
-				.amount(price.multiply(BigDecimal.valueOf(amount)))
-				.build();
+		return null;
 	}
 }
